9e1d45a65100011637d2b5f1d548862b4a3198e0
Sándor Balázs <it.sandor.balazs@gmail.com>
2021-04-16 18:13:41 +0200

    Update bezier_normal_point_shift.cpp

--- bezier_normal_point_shift.cpp
+++ bezier_normal_point_shift.cpp
@@ -60,7 +60,7 @@ void bezierCurve(int x[], int y[])
 	std::vector<std::pair<int, int>> centerPoints;
 	double xu = 0.0, yu = 0.0, u = 0.0;
 	int i = 0;
-	for (u = 0.0; u <= 1.0; u += 0.0001)
+	for (u = 0.0; u <= 1.0; u += 0.01)
 	{
 		xu = pow(1 - u, 3) * x[0] + 3 * u * pow(1 - u, 2) * x[1] + 3 * pow(u, 2) * (1 - u) * x[2]
 			+ pow(u, 3) * x[3];
